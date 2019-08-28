9aca2d291b254d45fe285d111ece1eba0d1c016e
Ollivdh <olivier.vandenheede.mailbox@gmail.com>
2019-08-28 16:25:21 +0200

    making the page responsive, adding flex, using map to fetch to pokemon names

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
+var input = document.getElementById("search");
+
+
 var pokeInfo = document.getElementById("poke-info");
 var pokeImg = document.getElementById("poke-img");
 
-var firstRequest = new XMLHttpRequest();
-firstRequest.open('GET', 'https://pokeapi.co/api/v2/pokemon/1/');
 
+// XMLHttpRequest
+var firstRequest = new XMLHttpRequest();
+firstRequest.open('GET', 'https://pokeapi.co/api/v2/generation/1/');
+
+// fetching pokemon species
 firstRequest.onload = function(){
     var pokeData = JSON.parse(firstRequest.responseText);
-     console.log(pokeData);
+     // console.log(pokeData.pokemon_species);
+
+    // using a map function to iterate over every date and console.log the pokemon's name.
+    pokeData.pokemon_species.map((pokemon)=>{
+        console.log(pokemon.name);
+    })
 };
 
 firstRequest.send();
+
+
+//scanning the api according to the input. "use e.target to capture the input field data from the form and then scan/filter pokeData.pokemon_species and display the corresponding data into the associated div".
+// input.addEventListener("keyup");
+
+
+
+
 
