bb0f68e2f5601e1a3b83519d2f478bd7b0f5ca33
Thijs Lambert <thijs.lamb@gmail.com>
2019-09-04 11:35:15 +0200

    remove eventlisteners

--- alternatief.js
+++ alternatief.js
@@ -4,48 +4,6 @@
   const input = document.querySelector('#search');
   const dropdown = document.querySelector('#dropdown');
 
-  //functies
-  const filterPokemon = (input) => {
-    const { pokemon_species } = JSON.parse(storage.getItem('pokedata'))
-
-    return pokemon_species.filter(pokemon => pokemon.name.includes(input))
-  }
-
-  const addToDropdown = (pokemon) => {
-    const pokeItem = document.createElement('li');
-    pokeItem.innerText = pokemon.name;
-
-    dropdown.appendChild(pokeItem);
-  }
-
-    
-  const createDropdown = (filteredPokemon) => {
-    dropdown.innerHTML = ``;
-    filteredPokemon.forEach(pokemon => addToDropdown(pokemon))
-  }
-
-  const handleInput = (e) => {
-    //check als er input is
-    if (e.currentTarget.value === ``) {
-      dropdown.innerHTML = ''; 
-      return
-    }
-
-    //telkens als er getypt wordt moet de data gefilterd worden + dropdown aanmaken
-    const filteredPokemon = filterPokemon(e.currentTarget.value)
-    createDropdown(filteredPokemon);
-
-    //als er op enter gedrukt wordt moet een resultaat verschijnen
-    if(e.key === "Enter"){
-      
-    }
-
-  }
-
-
-
-
-
   // init functie
   const init = () => {
     //data ophalen en opslaan in localstorage
@@ -57,5 +15,53 @@
     input.addEventListener('keyup', handleInput)
   };
 
+  // Event handlers
+  const handleClick = (e) => {
+    console.log(e.currentTarget.innerText)
+  };
+
+  const handleInput = (e) => {
+    //check als er input is
+    if (e.currentTarget.value === ``) {
+      removeDropdown()
+      return;
+    };
+
+    //telkens als er getypt wordt moet de data gefilterd worden + dropdown aanmaken
+    const filteredPokemon = filterPokemon(e.currentTarget.value.toLowerCase());
+    createDropdown(filteredPokemon);
+
+    //als er op enter gedrukt wordt moet een resultaat verschijnen
+    if(e.key === "Enter"){
+      console.log('Proficiat, je hebt op enter gedrukt.');
+    };
+  };
+
+  //functies
+  const filterPokemon = (input) => {
+    const { pokemon_species } = JSON.parse(storage.getItem('pokedata'));
+    return pokemon_species.filter(pokemon => pokemon.name.includes(input));
+  };
+
+  const addToDropdown = (pokemon) => {
+    const pokeItem = document.createElement('li');
+
+    pokeItem.innerText = pokemon.name;
+    pokeItem.addEventListener('click', handleClick);
+
+    dropdown.appendChild(pokeItem);
+  };
+
+  const createDropdown = (filteredPokemon) => {
+    filteredPokemon.forEach(pokemon => addToDropdown(pokemon));
+  };
+
+  const removeDropdown = () => {
+    dropdown.childNodes.forEach(element => element.removeEventListener('click', handleClick));
+    dropdown.innerHTML = ``;
+  }
+
+  
+  //GOGOGOOGOGOGO
   init();
 }
